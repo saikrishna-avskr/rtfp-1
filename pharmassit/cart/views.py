2c20e07afcfc7b041e13e3a18a5702f945654de8
saikrishna-avskr <saikrishnaavskr@gmail.com>
2024-05-24 00:11:25 +0530

    sai

--- pharmassit/cart/views.py
+++ pharmassit/cart/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render,redirect
-from django.template import loader
 from .models import Cart
 from products.models import Product
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 def add_to_cart(request,pid):
@@ -24,19 +21,31 @@ def remove_from_cart(request,pid):
 def show_cart(request):
     data = Cart.objects.all().values()
     dat = Cart.objects.values('product')
+    update_tot_price()
     to_find=[i['product'] for i in dat]
     new_data = Product.objects.filter(pid__in=to_find).values()
+    first_cart_item = Cart.objects.first()
+    if first_cart_item is not None:
+        tot_price = first_cart_item.tot_price
+        print(tot_price)
+    else:
+        tot_price="No items in Cart"
     context = {
         'cart': data,
         'quantity': new_data,
+        'tot_price': tot_price,
     }
     return render(request, 'cart.html', context)
 
 @require_POST
 def update_cart(request, product_id):
     new_quantity = request.POST.get('new_quantity')
+    price = Product.objects.get(pid=product_id).price
+    print(price)
+    new_price = int(new_quantity) * price
     cart_item = Cart.objects.get(pid=product_id)
     cart_item.quantity = new_quantity
+    cart_item.price = new_price
     cart_item.save()
     return redirect('cart:show_cart')
 
@@ -44,3 +53,10 @@ def clear_cart(request):
     Cart.objects.all().delete()
     messages.success(request, 'Cart cleared successfully')
     return redirect('cart:show_cart')
+
+def update_tot_price():
+    total = 0
+    for i in Cart.objects.all():
+        total += i.price
+    Cart.objects.all().update(tot_price=total)
+    
