6eb28b2dce45b0c647a67aa2c29cd6f2b749c111
saikrishna-avskr <saikrishnaavskr@gmail.com>
2025-07-20 14:43:00 +0530

    Add Whitenoise middleware for static file handling and update settings; modify build script to include migrate and collectstatic commands

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r pharmassit/requirements.txt
-
+python pharmassit/manage.py migrate
+python pharmassit/manage.py collectstatic --no-input
--- pharmassit/pharmassit/settings.py
+++ pharmassit/pharmassit/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,8 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
